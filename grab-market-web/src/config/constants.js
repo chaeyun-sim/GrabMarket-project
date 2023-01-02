b859a3db55667922e72c55ac8cdad4f81181dc44
chaeyun-sim <bysimune@gmail.com>
2023-01-02 17:40:17 +0900

    updated api url

--- grab-market-web/src/config/constants.js
+++ grab-market-web/src/config/constants.js
@@ -1,2 +1,4 @@
-export const API_URL = 'http://localhost:8080';
-//export const API_URL = 'https://grab-market-server.herokuapp.com'
+// export const API_URL = 'https://grab-market-server.onrender.com';
+// production API
+
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://grab-market-server.onrender.com' : 'http://localhost:8080'
