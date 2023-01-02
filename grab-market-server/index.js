19482ba549312aa6411501356c694f6c5ba8d66f
chaeyun-sim <bysimune@gmail.com>
2023-01-02 16:04:29 +0900

    make it better

--- grab-market-server/index.js
+++ grab-market-server/index.js
@@ -7,7 +7,6 @@ const multer = require('multer');
 const Product = require('./models/product')(sequelize, DataTypes);
 const Banner = require('./models/banner')(sequelize, DataTypes)
 const app = express();
-const port = process.env.PORT || 8080;
 
 const upload = multer({
   storage: multer.diskStorage({
@@ -132,8 +131,8 @@ app.post("/purchase/:id", (req, res) => {
 });
 
 
-app.listen(port, () => {
-   console.log("Server Connected to http://localhost:"+port);
+app.listen(process.env.PORT || 8080, () => {
+   console.log("Server Connected to http://localhost:"+app.settings.env);
    models.sequelize.sync().then(() => {
     console.log('DB Connected')
    }).catch((err) => {
