dda14d49d890c161dfff835cfcc8c8000c690c32
chaeyun-sim <bysimune@gmail.com>
2023-01-02 13:52:46 +0900

    added purchase part

--- grab-market-web/src/components/ProductsList.jsx
+++ grab-market-web/src/components/ProductsList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "antd";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { API_URL } from "../config/constants.js"
@@ -12,6 +13,9 @@ const Products = (props) => {
     const date = dayjs(product.createdAt).fromNow()
     return (
         <div className='product-card' key={product.id}>
+            {
+                product.soldout === 1 && <div className="product-blur"><h2>SOLD OUT</h2></div>
+            }
             <Link className="product-link" to={`/product/${product.id}`}>
                 <div>
                     <img className="product-img" src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
--- grab-market-web/src/products/index.js
+++ grab-market-web/src/products/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import axios from "axios";
 import dayjs from "dayjs";
+import { Button, message } from "antd";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "./index.css"
 import { API_URL } from "../config/constants.js"
@@ -11,30 +12,57 @@ dayjs.extend(relativeTime);  //확장 기능 호출
 const ProductsPage = () => {
     const [product, setProduct] = useState(null);
     const {id} = useParams();
+    const navigate = useNavigate();
 
-    useEffect(() => {
+    const getProduct = () => {
         axios.get(`${API_URL}/products/${id}`)
         .then((result) => { 
             setProduct(result.data.product)
         }).catch((err) => {
             console.error(err)
         })
+    };
+
+    useEffect(() => {
+        getProduct();
     }, []);
-    // console.log(product.id)
 
     if(product === null){
         return <h2 id="loading">상품 정보를 받고 있습니다....</h2>
-    }
+    };
 
-    console.log('')
+    const onClickPurchase = () => {
+        axios.post(`${API_URL}/purchase/${id}`).then((result) => {
+            message.info('구매가 완료되었습니다!')
+            getProduct();
+            navigate('/');
+        }).catch((err) => {
+            message.error(err.message)
+        })
+    };
+
     return (
         <div>
             <div id="image-box">
                 <img src={`${API_URL}/${product.imageUrl}`} alt="product" />
             </div>
-            <div id="profile-box">
-                <img src="/images/icons/avatar.png" alt="avatar" />
-                <span>{product.seller}</span>
+            <div id="overline">
+                <div id="purchase-box">
+                    <Button
+                        id="purchase-button"
+                        size="large"
+                        type="primary"
+                        danger
+                        onClick={onClickPurchase}
+                        disabled={product.soldout === 1 ? true : false}
+                    >
+                        {product.soldout === 0 ? "구매하기" : "품절"}
+                    </Button>
+                </div>
+                <div id="profile-box">
+                    <img src="/images/icons/avatar.png" alt="avatar" />
+                    <span>{product.seller}</span>
+                </div>
             </div>
             <div id="contents-box">
                 <div id="name">
